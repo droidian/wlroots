7712b023179f01aa6d266002995dcb69543c77c1
Erfan Abdi <erfangplus@gmail.com>
2020-08-31 01:31:18 +0430

    hwcomposer: Nuke EGL_PBUFFER_BIT leftover from headless

--- backend/hwcomposer/backend.c
+++ backend/hwcomposer/backend.c
@@ -81,14 +81,13 @@ struct wlr_backend *wlr_hwcomposer_backend_create(struct wl_display *display,
 
 	static EGLint config_attribs[] = {
 		EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
-		EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
+		EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
 		EGL_RED_SIZE, 8,
 		EGL_GREEN_SIZE, 8,
 		EGL_BLUE_SIZE, 8,
 		EGL_ALPHA_SIZE, 8,
 		EGL_DEPTH_SIZE, 24,
 		EGL_STENCIL_SIZE, 8,
-		EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
 		EGL_NONE
 	};
 
