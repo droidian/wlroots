ed14521afc08694b48cd172bff05bdf7ac8e2ebb
Eugenio Paolantonio (g7) <me@medesimo.eu>
2020-10-31 00:22:10 +0100

    backend: libinput: handle LIBINPUT_SWITCH_KEYPAD_SLIDE
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- backend/libinput/switch.c
+++ backend/libinput/switch.c
@@ -40,6 +40,9 @@ void handle_switch_toggle(struct libinput_event *event,
 	case LIBINPUT_SWITCH_TABLET_MODE:
 		wlr_event.switch_type = WLR_SWITCH_TYPE_TABLET_MODE;
 		break;
+	case LIBINPUT_SWITCH_KEYPAD_SLIDE:
+		wlr_event.switch_type = WLR_SWITCH_TYPE_KEYPAD_SLIDE;
+		break;
 	}
 	switch (libinput_event_switch_get_switch_state(sevent)) {
 	case LIBINPUT_SWITCH_STATE_OFF:
